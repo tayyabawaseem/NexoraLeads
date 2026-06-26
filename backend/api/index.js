b8913c754b80ccb9c43d7e7359c66f2ceb060e6a
Tayyaba Waseem <taiyabawaseem119@gmail.com>
2026-06-26 16:58:16 +0500

    vercel deployment changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,16 @@
 const serverless = require("serverless-http");
 const app = require("../app");
+const connectDB = require("../config/db");
 
-module.exports = serverless(app);
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+
+  return serverless(app)(req, res);
+};
+
+module.exports = handler;
